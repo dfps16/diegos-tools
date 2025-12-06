25477e7fa866c196ec0908db815c4d8674404455
Diego <dfps1u24@soton.ac.uk>
2025-12-06 12:03:36 +0000

    update Python SDK version to 3.14 and add altitude plotting functionality

--- Personal Projects/Orbits/MissionAnalyser.py
+++ Personal Projects/Orbits/MissionAnalyser.py
@@ -1,2 +1,15 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
 
+
+data = pd.read_csv("LunaAlt.txt", sep="   ")
+
+time, altitude = data.columns
+
+epoch = np.array(data[time])
+alt = np.array(data[altitude])
+
+plt.figure(1)
+plt.plot(epoch, alt)
+plt.show()
